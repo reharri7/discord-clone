f3710232adbee60b864d27ce1b27115b4015813b
Rhett Harrison <reharri7@asu.edu>
2024-03-23 20:25:27 -0700

    Add new UI components, configure theme and environment files
    Added new UI components and configured theme switching using TailwindCSS. Created a number of environment files specific to the IDEA development environment. Also included a middleware and configured .gitignore to avoid unneeded files.

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import {UserButton} from "@clerk/nextjs";
+import {ModeToggle} from "@/components/mode-toggle";
+
+
+export default function Home() {
+
+  return (
+    <div>
+      <UserButton
+      afterSignOutUrl="/"
+      />
+      <ModeToggle />
+    </div>
+  )
+}

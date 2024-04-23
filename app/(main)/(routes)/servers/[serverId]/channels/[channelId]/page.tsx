39269a9d40b986dedb782e884721ce90a949dc1e
Rhett Harrison <reharri7@asu.edu>
2024-04-22 21:27:37 -0700

    Add new components and enhance channel routing
    New components including Sheet, ChatHeader, and MobileToggle are introduced to improve UI. Also made enhancements in channel routing logic. Now it employs server database and profiles to fetch and display channel information, it also redirects unauthenticated users to the sign in page.

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,43 @@
-const ChannelIdPage = () => {
+import {db} from "@/lib/db";
+import {currentProfile} from "@/lib/current-profile";
+import {redirectToSignIn} from "@clerk/nextjs";
+import {ChatHeader} from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  }
+}
+
+const ChannelIdPage = async ({
+                               params
+                             }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    }
+  });
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    }
+  });
+
+  if (!channel || !member) {
+    return ("/");
+  }
+
   return (
-    <div>
-      ChannelId Page!
+    <div className={"bg-white dark:bg-[#313338] flex flex-col h-full"}>
+      <ChatHeader name={channel.name} serverId={channel.serverId} type={"channel"}/>
     </div>
   );
 }

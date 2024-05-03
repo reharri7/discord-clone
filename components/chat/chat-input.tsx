fdc42c3efcbcf931a8f8bb0e5cd1b39a35d259a0
Rhett Harrison <reharri7@asu.edu>
2024-05-02 21:03:53 -0700

    Add emoji picker to chat input
    This update introduces an emoji picker to the chat input component. We also added a popover component using "@radix-ui/react-popover" for the picker. Additionally, necessary dependencies such as "@emoji-mart/data", "@emoji-mart/react", and "emoji-mart" have been added to the package.json.

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -3,11 +3,13 @@ import * as z from "zod";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Form, FormControl, FormField, FormItem} from "../ui/form";
-import {Plus, Smile} from "lucide-react";
+import {Plus} from "lucide-react";
 import {Input} from "@/components/ui/input";
 import qs from "query-string";
 import axios from "axios";
 import {useModal} from "@/hooks/use-modal-store";
+import {EmojiPicker} from "@/components/emoji-picker";
+import {useRouter} from "next/navigation";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -27,6 +29,7 @@ export const ChatInput = ({
                             type,
                           }: ChatInputProps) => {
   const {onOpen} = useModal();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -45,6 +48,8 @@ export const ChatInput = ({
       });
 
       await axios.post(url, values);
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.error(error);
     }
@@ -70,7 +75,7 @@ export const ChatInput = ({
                   {...field}
                 />
                 <div className={"absolute top-7 right-8"}>
-                  <Smile/>
+                  <EmojiPicker onChange={(emoji: string) => field.onChange(`${field.value}${emoji}`)}/>
                 </div>
               </div>
             </FormControl>
